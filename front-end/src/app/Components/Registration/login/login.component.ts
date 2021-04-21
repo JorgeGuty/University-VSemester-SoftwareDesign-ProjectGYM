0ac27298f0dd9cb536fe5e08642fab67eeb31bd5
Alegarciy <ale.garciy@gmail.com>
2021-04-21 17:20:54 -0600

    U: Login and Register Interface Finished
    + Login Component
    + Register Component

--- front-end/src/app/Components/Registration/login/login.component.ts
+++ front-end/src/app/Components/Registration/login/login.component.ts
@@ -6,9 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  email: string = "";
+  log_email: string = "";
+  log_password: string = "";
+  reg_username: string = "";
+  reg_password: string = "";
+  reg_passwordConfirmation: string = "";
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onLogin() {
+    console.log("Se Logea");
+    console.log("Username: " + this.log_email);
+    console.log("Password: " + this.log_password);
+  }
+
+  onRegister() {
+    console.log("Se Registra");
+    console.log("Username: " + this.reg_username);
+    console.log("Password: " + this.reg_password);
+    console.log("Password Confirmation" + this.reg_passwordConfirmation);
+  }
 }
