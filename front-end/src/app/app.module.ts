4810c1042bb700c7fdaec4ecc50823bcf71b0dbb
Alegarciy <ale.garciy@gmail.com>
2021-04-21 23:18:13 -0600

    C: Created Routing
    + Created Navbar
    + Created Home
    + Created Profile

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,17 +9,38 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 /* Angular Material */
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 /* Angular Forms */
 import { FormsModule } from "@angular/forms";
-import { LoginComponent } from './Components/Registration/login/login.component';
+import { LoginComponent } from "./Components/Registration/login/login.component";
+import { NavbarComponent } from "./Components/General/navbar/navbar.component";
+import { HomeComponent } from "./Components/Home/home/home.component";
+import { ProfileComponent } from "./Components/Profile/profile/profile.component";
 
 /* Components */
 
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  {
+    path: "profile",
+    component: ProfileComponent,
+    /* , canActivate:[AuthGuard] */
+  },
+];
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -26,8 +48,11 @@ import { LoginComponent } from './Components/Registration/login/login.component'
     FormsModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
     MatInputModule,
     MatTabsModule,
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
