67501d28e1e8f4dc4c8c5cc149fbd59d5f276620
Alegarciy <ale.garciy@gmail.com>
2021-04-22 21:32:02 -0600

    U: Changes of Jorge Pulled

--- front-end/src/app/Services/Auth/auth.service.ts
+++ front-end/src/app/Services/Auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs";
 
 import { User } from "../../Models/Users/User";
 import { mixinColor } from "@angular/material/core";
+import { ConnectionsServices } from "../Connections/connectionsConstants";
 
 @Injectable({
   providedIn: "root",
@@ -38,21 +39,28 @@ export class AuthService {
   // }
 
   authenticateUser(userInfo: User) {
-    return this.httpClient.post("http://localhost:3000/login", userInfo, {
-      headers: { "Content-Type": "application/json" },
-    });
+    return this.httpClient.post(
+      ConnectionsServices.currentConnection + "/login",
+      userInfo,
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
 
   // TODO: register in API
-  // getProfile() {
-  //   let headers = new HttpHeaders();
-  //   this.loadToken();
-  //   headers.append("Authorization", this.authToken);
-  //   headers.append("Content-Type", "application/json");
-  //   return this.httpClient.get("http://localhost:3000/user/profile", {
-  //     headers,
-  //   });
-  // }
+  getProfile() {
+    let headers = new HttpHeaders();
+    this.loadToken();
+    headers.append("Authorization", this.authToken);
+    headers.append("Content-Type", "application/json");
+    return this.httpClient.get(
+      ConnectionsServices.currentConnection + "/user/profile",
+      {
+        headers,
+      }
+    );
+  }
 
   storeUserData(token: string, user: User) {
     localStorage.setItem("id_token", token);
