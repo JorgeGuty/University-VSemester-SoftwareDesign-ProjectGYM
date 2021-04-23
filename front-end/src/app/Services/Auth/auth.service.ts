3028dd7600a35c78f1b6bce5879a6c5cb043e1b1
Alegarciy <ale.garciy@gmail.com>
2021-04-22 18:52:21 -0600

    + U: AuthLogin Finished
    + Local Storage of token and user
    + Navbar logout manager
    + Started with Auth verification

--- front-end/src/app/Services/Auth/auth.service.ts
+++ front-end/src/app/Services/Auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 import { User } from "../../Models/Users/User";
+import { mixinColor } from "@angular/material/core";
 
 @Injectable({
   providedIn: "root",
@@ -10,14 +11,14 @@ import { User } from "../../Models/Users/User";
 export class AuthService {
   private isAuthenticated: boolean;
   private currentUser: User;
-  private token: string;
+  private authToken: string;
   private user: any;
 
   constructor(private httpClient: HttpClient) {
     this.isAuthenticated = false;
-    this.token = "";
+    this.authToken = "";
     this.currentUser = {
-      _id: "",
+      id: "",
       username: "",
       password: "",
     };
@@ -36,9 +37,39 @@ export class AuthService {
   //   );
   // }
 
-  authenticateUser(userInfo: any) {
+  authenticateUser(userInfo: User) {
     return this.httpClient.post("http://localhost:3000/login", userInfo, {
       headers: { "Content-Type": "application/json" },
     });
+  }
+
+  // TODO: register in API
+  // getProfile() {
+  //   let headers = new HttpHeaders();
+  //   this.loadToken();
+  //   headers.append("Authorization", this.authToken);
+  //   headers.append("Content-Type", "application/json");
+  //   return this.httpClient.get("http://localhost:3000/user/profile", {
+  //     headers,
+  //   });
+  // }
+
+  storeUserData(token: string, user: User) {
+    localStorage.setItem("id_token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem("id_token");
+    if (token != null) this.authToken = token;
+  }
+
+  logout() {
+    //null authToken is ""
+    this.authToken = "";
+    this.user = null;
+    localStorage.clear();
   }
 }
