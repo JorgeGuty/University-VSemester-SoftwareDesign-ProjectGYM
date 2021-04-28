7452cb8d75df370b56b23f5f25db255981f752e7
Alegarciy <ale.garciy@gmail.com>
2021-04-28 11:29:11 -0600

    U: Auth Services Routes Finished
    + Verification System completed

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+/* Routes */
+import { AdminDashboardComponent } from "./Components/Dashboards/admin-dashboard/admin-dashboard.component";
+import { ClientDashboardComponent } from "./Components/Dashboards/client-dashboard/client-dashboard.component";
+import { HomeComponent } from "./Components/Home/home/home.component";
+import { ProfileComponent } from "./Components/Profile/profile/profile.component";
+import { LoginComponent } from "./Components/Registration/login/login.component";
+
+/* Services */
+import { AuthAdminGuard } from "./Guards/authAdmin.guard";
+import { AuthUserGuard } from "./Guards/authUser.guard";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  {
+    path: "profile",
+    component: ProfileComponent,
+    canActivate: [AuthUserGuard],
+  },
+  {
+    path: "admin/adminDashboard",
+    component: AdminDashboardComponent,
+    canActivate: [AuthAdminGuard],
+  },
+  {
+    path: "client/clientDashboard",
+    component: ClientDashboardComponent,
+    canActivate: [AuthUserGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
