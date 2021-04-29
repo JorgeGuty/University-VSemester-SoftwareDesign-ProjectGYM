a0da9320d29f45d922702ceb8e2dbe3106fad985
Eduardo <edumadrim@gmail.com>
2021-04-28 19:39:53 -0600

    U: Join both insertion files

--- SQLScripts/DataInsertion.sql
+++ SQLScripts/DataInsertion.sql
@@ -1,6 +1,55 @@
+USE PlusGymProject
 
-insert into dbo.TipoUsuario VALUES ('administrador'),('cliente')
-insert into dbo.TipoInstructor VALUES ('planta'),('temporal')
+INSERT INTO FormaDePago (Id, Nombre)
+VALUES (1, 'Tarjeta');
+
+INSERT INTO TipoUsuario (Id, Nombre)
+VALUES (1, 'Administrador');
+INSERT INTO TipoUsuario (Id, Nombre)
+VALUES (2, 'Cliente');
+
+INSERT INTO dbo.TipoInstructor VALUES ('planta'),('temporal');
+
+INSERT INTO TipoMovimiento (Id, Nombre)
+VALUES (1, 'Credito');
+
+INSERT INTO Usuario (Username, [Password], IdTipoUsuario)
+VALUES ('Admin1',1234,1);
+INSERT INTO Usuario (Username, [Password], IdTipoUsuario)
+VALUES ('Cliente1',1234,2);
+
+INSERT INTO UsuarioAdmin (Id, Nombre)
+VALUES (1, 'Jorge El Curioso');
+-- Id varia segun tabla Usuario
+
+INSERT INTO Cliente (Cedula,Nombre,Correo,Celular)
+VALUES ('1100','Popeye','popeyeElMarino@gmail.com','60009999');
+
+insert into
+    dbo.Cliente (Cedula, Nombre, Correo, Celular, Saldo, Estado)
+VALUES
+    ('118090772', 'Elclien T. Rodriguez', 'aaa@a.gmail', '+506 70560910', 0.0, 1)
+
+INSERT INTO UsuarioCliente (Id, IdCliente)
+VALUES (2, 1);
+
+INSERT INTO dbo.UsuarioCliente (Id, IdCliente)
+VALUES(3,2);
+-- Id varia segun tabla Usuario
+
+INSERT INTO Movimiento (Monto, Fecha, IdCliente,IdTipoMovimiento)
+VALUES (1000,GETDATE(),1,1);
+INSERT INTO Movimiento (Monto, Fecha, IdCliente,IdTipoMovimiento)
+VALUES (2000,GETDATE(),1,1);
+
+UPDATE Cliente
+SET Saldo=3000
+WHERE Id=1
+
+INSERT INTO Credito (Id,IdFormaDePago)
+VALUES (1,1);
+INSERT INTO Credito (Id,IdFormaDePago)
+VALUES (2,1);
 
 
 insert into 
@@ -28,28 +77,3 @@ values
     ('Sesion de Yoga', CONVERT(DATETIME, '2021-04-25 14:00:00'), 120, 12, 5000.00, 1, 2, 2, 1),
     ('Sesion de Yoga', CONVERT(DATETIME, '2021-04-26 16:00:00'), 120, 12, 5000.00, 1, 3, 1, 1)
 
-
-insert into
-    dbo.Usuario (Username, [Password], TipoUsuario, Estado)
-VALUES
-    ('Cliente1', '1234', 2, 1),
-    ('Admin1', '1234', 1, 1)
-
-insert into
-    dbo.UsuarioAdmin (Nombre)
-VALUES
-    (42312)    
-
-insert into
-    dbo.Cliente (Cedula, Nombre, Correo, Celular, Saldo, Estado)
-VALUES
-    ('118090772', 'Elclien T. Rodriguez', 'aaa@a.gmail', '+506 70560910', 0.0, 1)
-       
-
-insert into
-    dbo.UsuarioCliente (ClientId)
-VALUES
-    (1)    
-
-
-EXEC SP_GetUserByUsername 'Cliente1'
