28439666ffedf4b7219a2dd8643620017e00a128
Jorge Gutiérrez Cordero <jguty2001@gmail.com>
2021-04-30 20:43:03 -0600

    U: routes additions

--- API/WebServer/routes.go
+++ API/WebServer/routes.go
@@ -10,13 +10,19 @@ func Setup(app *fiber.App){
 
 	general := app.Group("/general")
 	client := app.Group("/client")
-	//admin := app.Group("/admin")
+	admin := app.Group("/admin")
 
 	general.Post("/login", Controllers.Login)
 	general.Get("/activeSchedule", Controllers.GetActiveSchedule)
 
 	client.Get("/userInfo", Controllers.GetUserInfo)
 	client.Get("/reservedSessions", Controllers.GetReservedSessions)
+	client.Post("/bookSession", Controllers.BookSession)
+
+	admin.Get("/preliminarySchedule", Controllers.GetPreliminarySchedule)
+	admin.Post("/insertPreliminarySession", Controllers.InsertPreliminarySession)
+	admin.Post("/deletePreliminarySession", Controllers.DeletePreliminarySession)
+	admin.Post("/confirmPreliminarySchedule", Controllers.ConfirmPreliminarySchedule)
 }
 
 
