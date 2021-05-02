10b4aefa2f5128e2c7c2073a0f337450c531fec0
Eduardo <edumadrim@gmail.com>
2021-05-01 18:11:34 -0600

    U: complete getCurrentCalendar & added tests

--- SQLScripts/StoredProcedures/SP_getCurrentCalendar.sql
+++ SQLScripts/StoredProcedures/SP_getCurrentCalendar.sql
@@ -7,16 +7,30 @@ CREATE OR ALTER PROCEDURE dbo.SP_getCurrentCalendar
 AS
     BEGIN
         DECLARE @StartDate Date;
-        DECLARE @EndDate Date;
+        SET @StartDate = GETDATE();
 
-        SET @EndDate = EOMONTH(GETDATE());
-        SET @StartDate = DATEADD(DAY,1,EOMONTH(@EndDate,-1));
-
-        SELECT Id, Nombre, Fecha, HoraInicio, DuracionMinutos, Costo, Cancelada
-        FROM 
-            dbo.Sesion 
-            WHERE 
-                Fecha >= @StartDate 
-                AND
-                Fecha <= @EndDate;
+        SELECT 
+            s.Id, 
+            s.Nombre AS name,
+            s.Fecha                              AS [date],
+            s.HoraInicio                         AS startTime,
+            s.DuracionMinutos                    AS duration,
+            ISNULL((s.Cupo - r.Reservas),s.Cupo) AS availableSpaces,
+            s.Costo                              AS cost,
+            s.Cancelada                          AS isCanceled,
+            i.Nombre                             AS instructor, 
+            es.Nombre                            AS Service
+        FROM dbo.Sesion s
+        INNER JOIN dbo.Especialidades es
+            ON es.Id = s.EspecialidadId
+        INNER JOIN dbo.Instructor i 
+            ON i.Id = s.InstructorId
+        LEFT JOIN
+            (
+                SELECT SesionId, COUNT(SesionId) AS Reservas 
+                    FROM dbo.Reserva
+                    GROUP BY SesionId
+            ) AS r
+            ON r.SesionId = s.Id
+        WHERE Fecha >= @StartDate
     END
