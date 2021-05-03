1db926fadfadefa4e924ad39742d938134a09a8b
Alegarciy <ale.garciy@gmail.com>
2021-05-03 16:08:02 -0600

    A: Form Display

--- front-end/src/app/Components/Dashboards/Admin_Preliminary/admin-preliminary-dialog/admin-preliminary-dialog.component.ts
+++ front-end/src/app/Components/Dashboards/Admin_Preliminary/admin-preliminary-dialog/admin-preliminary-dialog.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-admin-preliminary-dialog',
-  templateUrl: './admin-preliminary-dialog.component.html',
-  styleUrls: ['./admin-preliminary-dialog.component.scss']
+  selector: "app-admin-preliminary-dialog",
+  templateUrl: "./admin-preliminary-dialog.component.html",
+  styleUrls: ["./admin-preliminary-dialog.component.scss"],
 })
 export class AdminPreliminaryDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSave() {
+    console.log("Clase creada");
   }
-
 }
+
+// @Component({
+//   selector: 'dialog-overview-example-dialog',
+//   templateUrl: 'dialog-overview-example-dialog.html',
+// })
+// export class DialogOverviewExampleDialog {
+
+//   constructor(
+//     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+//     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+//   onNoClick(): void {
+//     this.dialogRef.close();
+//   }
+
+// }
