261e01fe36f4423e9b2947f00fe0d18178b8e30a
Jorge Gutiérrez Cordero <jguty2001@gmail.com>
2021-05-03 18:22:36 -0600

    U: SP_GetClientProfileInfo implementation

--- SQLScripts/SP_GetClientProfileInfo.sql
+++ SQLScripts/SP_GetClientProfileInfo.sql
@@ -1,13 +1,41 @@
+-- Create a new stored procedure called 'StoredProcedureName' in schema 'dbo'
+-- Drop the stored procedure if it already exists
+IF EXISTS (
+SELECT *
+    FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_SCHEMA = N'dbo'
+    AND SPECIFIC_NAME = N'SP_GetClientProfileInfo'
+    AND ROUTINE_TYPE = N'PROCEDURE'
+)
+DROP PROCEDURE dbo.SP_GetClientProfileInfo
+GO
 -- Create the stored procedure in the specified schema
-CREATE OR ALTER PROCEDURE dbo.SP_GetClientProfileInfo
+CREATE PROCEDURE dbo.SP_GetClientProfileInfo
     @pUsername  NVARCHAR(50)
--- add more stored procedure parameters here
 AS
 BEGIN
     -- body of the stored procedure
-    SELECT @param1, @param2
+    SELECT 
+        [user].Username AS  [Username],
+        [user].Id         AS  [UserId],
+        client.Id       AS  [ClientId],
+        client.Nombre   AS  [Name],
+        client.Correo   AS  [Email],
+        client.Celular  AS  [PhoneNumber],
+        client.Cedula   AS  [Identification],
+        client.Saldo    AS  [Balance]
+    FROM 
+        dbo.Usuario AS [user]
+    INNER JOIN 
+        dbo.UsuarioCliente AS clientUser
+        ON [user].Id = clientUser.Id
+    INNER JOIN 
+        dbo.Cliente AS client
+        ON clientUser.ClienteId = client.Id
+    WHERE 
+        [user].Username = @pUsername
 END
 GO
 -- example to execute the stored procedure we just created
-EXECUTE dbo.StoredProcedureName 1 /*value_for_param1*/, 2 /*value_for_param2*/
+EXECUTE dbo.SP_GetClientProfileInfo 'Cliente2'
 GO
