9255208d80c5f8f8be724d7d6ecfb5a4a15567a5
Eduardo <edumadrim@gmail.com>
2021-05-06 07:34:13 -0600

    U: Added route for testing

--- API/WebServer/routes.go
+++ API/WebServer/routes.go
@@ -7,6 +7,7 @@ import (
 
 func Setup(app *fiber.App){
 	app.Get("/", Controllers.Start)
+	app.Get("/test", Controllers.SqlTests)
 
 	general := app.Group("/general")
 	client := app.Group("/client")
