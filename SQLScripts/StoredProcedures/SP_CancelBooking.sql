70de57bc14ce24f670259ab0b0a25268f498cebf
Jorge Gutiérrez Cordero <jguty2001@gmail.com>
2021-05-06 17:25:44 -0600

    U: SP corrections

--- SQLScripts/StoredProcedures/SP_CancelBooking.sql
+++ SQLScripts/StoredProcedures/SP_CancelBooking.sql
@@ -18,8 +18,12 @@ AS
 BEGIN
     BEGIN TRY
 
-        DECLARE @ClientID           INT
-        DECLARE @ReturnCode         INT
+        DECLARE @ClientID           INT                
+        DECLARE @NotBookedErrorCode INT
+        DECLARE @SPErrorCode        INT     
+
+        SET @NotBookedErrorCode = -5001
+        SET @SPErrorCode = -1                   
 
         -- Gets client id from the username
         SET @ClientID = 
@@ -34,7 +38,7 @@ BEGIN
 
         -- Checks if client has not booked that session
         IF @ClientID NOT IN ( SELECT booking.ClienteId FROM dbo.Reserva AS booking WHERE booking.SesionId = @pSessionID AND booking.Activa = 1)
-            RAISERROR (N'Session not booked by user %s', 11, 1, @pUsername);  
+            RETURN @NotBookedErrorCode 
         ELSE
             BEGIN 
                 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
@@ -53,8 +57,7 @@ BEGIN
     BEGIN CATCH
         IF @@TRANCOUNT > 0
             ROLLBACK
-        SELECT ERROR_MESSAGE() AS ErrorMessage; 
-        RETURN -1
+        RETURN @SPErrorCode 
     END CATCH
 END
 GO
