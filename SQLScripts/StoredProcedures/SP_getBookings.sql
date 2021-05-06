e16c7d5f0260f8df27fd25e239e53631cb83526f
Eduardo <edumadrim@gmail.com>
2021-05-05 21:01:25 -0600

    A: getBookings sql script

---
+++ SQLScripts/StoredProcedures/SP_getBookings.sql
@@ -0,0 +1,53 @@
+-- =============================================
+-- Author:		Eduardo Madrigal Marin
+-- Description:	Retrieves the sesions of the current month
+-- =============================================
+
+CREATE OR ALTER PROCEDURE dbo.SP_getBookings
+    @pUsername  VARCHAR(50)
+AS
+    BEGIN
+        DECLARE @StartDate Date;
+        DECLARE @ClientId int;
+        SET @StartDate = GETDATE();
+
+        SELECT @ClientId = ClientId
+            FROM dbo.CompleteClients cc
+            WHERE cc.Username = @pUsername;
+
+        SELECT 
+            cs.SessionID,
+            cs.Name                                    AS SessionName,
+            cs.SessionDate,
+            cs.StartTime,
+            cs.Duration,
+            ISNULL((cs.Spaces - r.Bookings), cs.Spaces) AS AvailableSpaces,
+            cs.Cost,
+            cs.IsCancelled,
+            cs.InstructorName,
+            cs.InstructorIdentification,
+            cs.InstructorEmail,
+            cs.InstructorType,
+            cs.ServiceName
+        FROM dbo.CompleteSessions cs
+        INNER JOIN
+            (
+                SELECT SesionId
+                    FROM dbo.Reserva
+                    WHERE 
+                        ClienteId = @ClientId
+                        AND 
+                        Activa = 1
+
+            ) AS clientSessions
+        ON clientSessions.SesionId = cs.SessionID
+        INNER JOIN
+            (
+                SELECT SesionId, COUNT(SesionId) AS Bookings 
+                    FROM dbo.Reserva
+                    GROUP BY SesionId
+            ) AS r
+            ON r.SesionId = cs.SessionID
+        WHERE 
+        cs.SessionDate >= @StartDate 
+    END
