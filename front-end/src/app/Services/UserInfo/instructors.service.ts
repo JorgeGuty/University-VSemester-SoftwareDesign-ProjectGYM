1cf9f7530860af4f5dcb6b92813533cff0ac3903
Alegarciy <ale.garciy@gmail.com>
2021-05-07 12:25:26 -0600

    C: Instructor Service
    A: GetRefisteredInstructors

---
+++ front-end/src/app/Services/UserInfo/instructors.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthService } from "../Auth/auth.service";
+import ConnectionsServices from "../Connections/connectionsConstants";
+
+@Injectable({
+  providedIn: "root",
+})
+export class InstructorsService {
+  constructor(private httpClient: HttpClient) {}
+
+  getRegisteredInstructors(): Observable<any> {
+    return this.httpClient.post(
+      ConnectionsServices.currentConnection + "/general/instructors",
+      { filterByService: "0", service: "", filterByType: "0", type: "" },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${AuthService.getAuthToken()}`,
+        },
+      }
+    );
+  }
+}
