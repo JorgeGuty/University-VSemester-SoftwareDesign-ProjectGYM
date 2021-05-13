c88f5239926ac373b00500affbdc1e0f632dc1ab
Alegarciy <ale.garciy@gmail.com>
2021-05-12 23:14:07 -0600

    U: Preliminary schedule

--- front-end/src/app/Components/Dashboards/Admin_Preliminary/admin-preliminary-dashboard/admin-preliminary-dashboard.component.ts
+++ front-end/src/app/Components/Dashboards/Admin_Preliminary/admin-preliminary-dashboard/admin-preliminary-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ɵɵsetComponentScope } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { AdminPreliminaryDialogComponent } from "../admin-preliminary-dialog/admin-preliminary-dialog.component";
 import { AdminScheduleService } from "src/app/Services/Dashboard/admin-schedule.service";
@@ -80,6 +80,14 @@ export class AdminPreliminaryDashboardComponent implements OnInit {
         this.scheduleMap.set(keyDay, dayOfWeek);
       }
     }
+  }
+
+  confirmSchedule() {
+    this.adminScheduleService
+      .confirmPreliminarySchedule(this.dateJSON)
+      .subscribe(() => {
+        console.log("Calendar confirmation");
+      });
   }
 
   reload() {
