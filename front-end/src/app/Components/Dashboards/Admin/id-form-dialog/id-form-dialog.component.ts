85d28cf3815257bbb5ee1fc7380c1b51aa04dd0d
Alegarciy <ale.garciy@gmail.com>
2021-05-14 11:05:43 -0600

    C: Dialog Form

---
+++ front-end/src/app/Components/Dashboards/Admin/id-form-dialog/id-form-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-id-form-dialog',
+  templateUrl: './id-form-dialog.component.html',
+  styleUrls: ['./id-form-dialog.component.scss']
+})
+export class IdFormDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
