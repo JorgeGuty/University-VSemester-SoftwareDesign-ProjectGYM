2d84e4720b1112a404340bb658f5de9092f936b4
Alegarciy <ale.garciy@gmail.com>
2021-05-14 13:04:56 -0600

    A: Dialog Box for Admin

--- front-end/src/app/Components/Dashboards/Admin/id-form-dialog/id-form-dialog.component.ts
+++ front-end/src/app/Components/Dashboards/Admin/id-form-dialog/id-form-dialog.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-id-form-dialog',
-  templateUrl: './id-form-dialog.component.html',
-  styleUrls: ['./id-form-dialog.component.scss']
+  selector: "app-id-form-dialog",
+  templateUrl: "./id-form-dialog.component.html",
+  styleUrls: ["./id-form-dialog.component.scss"],
 })
 export class IdFormDialogComponent implements OnInit {
+  clientId: string = "";
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<IdFormDialogComponent>) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onConfirm() {
+    this.dialogRef.close(this.clientId);
   }
-
 }
