e72cccce0cf4eca7d4d062b79a355d6578eee215
Alegarciy <ale.garciy@gmail.com>
2021-05-15 17:49:04 -0600

    R: Tables refactor

--- front-end/src/app/Components/Tables/instructor-table/instructor-table.component.ts
+++ front-end/src/app/Components/Tables/instructor-table/instructor-table.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import Instructor from "src/app/Models/Schedule/Instructor";
+import { InstructorsService } from "src/app/Services/UserInfo/instructors.service";
 
 @Component({
-  selector: 'app-instructor-table',
-  templateUrl: './instructor-table.component.html',
-  styleUrls: ['./instructor-table.component.scss']
+  selector: "app-instructor-table",
+  templateUrl: "./instructor-table.component.html",
+  styleUrls: ["./instructor-table.component.scss"],
 })
 export class InstructorTableComponent implements OnInit {
+  @Input()
+  instructor!: any;
+  @Input()
+  columnContent!: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDelete(instructor: Instructor) {
+    // TODO: Implement delete Instructor service
+    console.log(instructor);
   }
-
 }
