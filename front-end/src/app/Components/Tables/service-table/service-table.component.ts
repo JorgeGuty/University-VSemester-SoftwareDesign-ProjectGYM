0ae93ac1a5828b7c4b85e507be1a2208f86ac1fe
Alegarciy <ale.garciy@gmail.com>
2021-05-15 18:07:27 -0600

    R: Services table

--- front-end/src/app/Components/Tables/service-table/service-table.component.ts
+++ front-end/src/app/Components/Tables/service-table/service-table.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import Service from "src/app/Models/Schedule/Service";
 
 @Component({
-  selector: 'app-service-table',
-  templateUrl: './service-table.component.html',
-  styleUrls: ['./service-table.component.scss']
+  selector: "app-service-table",
+  templateUrl: "./service-table.component.html",
+  styleUrls: ["./service-table.component.scss"],
 })
 export class ServiceTableComponent implements OnInit {
+  @Input()
+  services!: any;
+  @Input()
+  columnContent!: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDelete(service: Service) {
+    // TODO: Implement delete Instructor service
+    console.log(service);
   }
-
 }
