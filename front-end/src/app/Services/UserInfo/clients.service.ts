d6c5ba4db205bd4a4e8fceeeaa1f879c2330f287
Alegarciy <ale.garciy@gmail.com>
2021-05-23 13:43:41 -0600

    A: Client Service Get Info

--- front-end/src/app/Services/UserInfo/clients.service.ts
+++ front-end/src/app/Services/UserInfo/clients.service.ts
@@ -1,41 +1,25 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { JwtHelperService } from "@auth0/angular-jwt";
-
-import { Session } from "../../Models/Schedule/Session";
 import { AuthService } from "../Auth/auth.service";
 import ConnectionsServices from "../Connections/connectionsConstants";
-import User from "src/app/Models/Users/User";
 
 @Injectable({
   providedIn: "root",
 })
-export class ClientScheduleService {
+export class ClientsService {
   constructor(
     private httpClient: HttpClient,
     private authService: AuthService
   ) {}
 
-  getCurrentSessionSchedule(): Observable<any> {
-    return this.httpClient.get(
-      ConnectionsServices.currentConnection + "/general/activeSchedule",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${AuthService.getAuthToken()}`,
-        },
-      }
-    );
-  }
-
-  getReservedSessions(): Observable<any> {
+  getClientInfo(): Observable<any> {
     let user = this.authService.getCurrentUser();
     if (user != undefined && user.identifier != undefined) {
       var userForm: any = { clientIdentification: user.identifier.toString() };
     }
     return this.httpClient.post(
-      ConnectionsServices.currentConnection + "/client/reservedSessions",
+      ConnectionsServices.currentConnection + "/client/clientInfo",
       userForm,
       {
         headers: {
