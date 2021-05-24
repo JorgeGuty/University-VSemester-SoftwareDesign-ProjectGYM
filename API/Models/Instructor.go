caa625e57b1c14670561383b99579ac03b60dba3
Alegarciy <ale.garciy@gmail.com>
2021-05-23 23:21:34 -0600

    U: Changes

--- API/Models/Instructor.go
+++
@@ -1,9 +0,0 @@
-package Models
-
-type Instructor struct {
-	ID             int    `json:"id"`
-	Name           string `json:"name"`
-	Identification string `json:"identification"`
-	Email          string `json:"email"`
-	Type           string `json:"type"`
-}
--- API/Models/User.go
+++
@@ -1,9 +0,0 @@
-package Models
-
-type Login struct {
-	Identifier int    `json:"identifier"`
-	Username   string `json:"username"`
-	Type       int    `json:"type"`
-	Password   string `json:"-"`
-	Token      string `json:"token"`
-}
