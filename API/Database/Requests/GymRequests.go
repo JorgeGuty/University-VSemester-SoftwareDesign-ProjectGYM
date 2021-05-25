2f94dc0328a627879340a20013f1999a21f1daf2
Alegarciy <ale.garciy@gmail.com>
2021-05-25 15:14:03 -0600

    Revert "Merge branch 'Ale/Dev_Ale' into dev"
    This reverts commit 9e6fc917bbef96263a0bcec757db6256cf1c5a67.

--- API/Database/Requests/GymRequests.go
+++ API/Database/Requests/GymRequests.go
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 package Requests
+=======
+package Requests
+>>>>>>> parent of 9e6fc91... Merge branch 'Ale/Dev_Ale' into dev
---
+++ API/Models/Instructor.go
@@ -0,0 +1,9 @@
+package Models
+
+type Instructor struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	Identification string `json:"identification"`
+	Email          string `json:"email"`
+	Type           string `json:"type"`
+}
