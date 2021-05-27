e6ac10b3e9e851ae145aab737b9845e4c4530b94
Alegarciy <ale.garciy@gmail.com>
2021-05-27 00:58:05 -0600

    A: Client Service
    + Delete not working yet

--- front-end/src/app/Components/Tables/client-table/client-table.component.ts
+++ front-end/src/app/Components/Tables/client-table/client-table.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from "@angular/core";
+import Client from "src/app/Models/Clients/Client";
 
 @Component({
-  selector: 'app-client-table',
-  templateUrl: './client-table.component.html',
-  styleUrls: ['./client-table.component.scss']
+  selector: "app-client-table",
+  templateUrl: "./client-table.component.html",
+  styleUrls: ["./client-table.component.scss"],
 })
 export class ClientTableComponent implements OnInit {
+  @Input()
+  client!: any;
+  @Input()
+  columnContent!: any;
+  @Output()
+  clientDeleted = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDelete(client: Client) {
+    this.clientDeleted.emit(client);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }
