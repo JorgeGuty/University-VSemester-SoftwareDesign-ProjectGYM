fd65b7bb1f6ae8dff2c3420c7182c09122f20a53
Eduardo <edumadrim@gmail.com>
2021-06-01 14:25:40 -0600

    A: GetInstructorInfo

---
+++ SQLScripts/StoredProcedures/SP_GetInstructorInfo.sql
@@ -0,0 +1,41 @@
+-- =============================================
+-- Author:		Eduardo Madrigal Marin
+-- Description:	Get instructor info
+-- =============================================
+
+ALTER PROCEDURE dbo.SP_GetInstructorInfo
+    @pInstructorNumber  INT
+AS
+    BEGIN
+        DECLARE @SPErrorCode                   INT
+        DECLARE @InstructorNotFoundErrorCode   INT
+        DECLARE @InstructorID                  INT
+        
+        BEGIN TRY
+            SET @SPErrorCode                 = -50001;
+            SET @InstructorNotFoundErrorCode = -50008;
+            
+            SELECT @InstructorID = Id 
+            FROM Instructor
+            WHERE Id = @pInstructorNumber;
+            
+            IF @InstructorID IS NULL
+                RETURN @InstructorNotFoundErrorCode;
+
+            SELECT  InstructorID, 
+                    InstructorName,
+                    InstructorEmail,
+                    InstructorIdentification,
+                    [Type]
+            FROM CompleteInstructors
+            WHERE
+                Active = 1
+                AND
+                InstructorID = @pInstructorNumber;
+
+            RETURN 1
+        END TRY
+        BEGIN CATCH
+            RETURN -@@Error;
+        END CATCH
+    END
