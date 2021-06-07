5732fd10f72ddde3ca301cae5d8b8e7357877ce7
Eduardo <edumadrim@gmail.com>
2021-06-06 22:40:48 -0600

    U: SetRoomWorkingHours token auth

--- API/WebServer/Controllers/roomController.go
+++ API/WebServer/Controllers/roomController.go
@@ -31,11 +31,11 @@ func SetRoomMaxSpace(context *fiber.Ctx) error {
 
 func SetRoomWorkingHours(context *fiber.Ctx) error {
 
-	// token := Common.AnalyzeToken(context)
+	token := Common.AnalyzeToken(context)
 
-	// if token == nil {
-	// 	return nil
-	// }
+	if token == nil {
+		return nil
+	}
 
 	var data map[string]string
 	if err := context.BodyParser(&data); err != nil {
