b6f95b209746f53d5550a80ab8b51e03e09136c2
Jorge Gutiérrez Cordero <jguty2001@gmail.com>
2021-06-22 23:25:34 -0600

    U: strategies implementations

--- API/WebServer/Controllers/FilteredScheduleStrategy/FilterByDate.go
+++ API/WebServer/Controllers/FilteredScheduleStrategy/FilterByDate.go
@@ -1,1 +1,13 @@
 package FilteredScheduleStrategy
+
+import (
+	"API/Database/Requests"
+	"API/Models"
+)
+
+type FilterByDate struct{
+}
+
+func (strategy *FilterByDate) filter(parameter string) Models.Schedule {
+	return Requests.GetSessionsByDate(parameter)
+}
