2f9f6f2b11986d346d00ba6ea2af9a166e406eff
Jorge Gutiérrez Cordero <jguty2001@gmail.com>
2021-06-22 23:17:36 -0600

    U: request functions addition

--- API/Database/Requests/SessionRequests.go
+++ API/Database/Requests/SessionRequests.go
@@ -21,6 +21,62 @@ func GetCurrentSessionSchedule() Models.Schedule {
 	return schedule
 }
 
+func GetSessionsByInstructor(pInstructorName string) Models.Schedule {
+	query := fmt.Sprintf(`EXEC SP_GetSessionsByInstructor %d;`, pInstructorName)
+
+	resultSet, err := Database.ReadTransaction(query)
+
+	if err != nil {
+		return Models.Schedule{}
+	}
+
+	schedule := ParseSchedule(resultSet)
+
+	return schedule
+}
+
+func GetSessionsByServiceType(pServiceType string) Models.Schedule {
+	query := fmt.Sprintf(`EXEC SP_GetSessionsByServiceType %d;`, pServiceType)
+
+	resultSet, err := Database.ReadTransaction(query)
+
+	if err != nil {
+		return Models.Schedule{}
+	}
+
+	schedule := ParseSchedule(resultSet)
+
+	return schedule
+}
+
+func GetSessionsByDate(pDate string) Models.Schedule {
+	query := fmt.Sprintf(`EXEC SP_GetSessionsByDate %d;`, pDate)
+
+	resultSet, err := Database.ReadTransaction(query)
+
+	if err != nil {
+		return Models.Schedule{}
+	}
+
+	schedule := ParseSchedule(resultSet)
+
+	return schedule
+}
+
+func GetSessionsByTime(pTime string) Models.Schedule {
+	query := fmt.Sprintf(`EXEC SP_GetSessionsByTime %d;`, pTime)
+
+	resultSet, err := Database.ReadTransaction(query)
+
+	if err != nil {
+		return Models.Schedule{}
+	}
+
+	schedule := ParseSchedule(resultSet)
+
+	return schedule
+}
+
 func GetReservedSessions(pMembershipNumber int) Models.Schedule {
 
 	query := fmt.Sprintf(`EXEC SP_GetBookings %d;`, pMembershipNumber)
