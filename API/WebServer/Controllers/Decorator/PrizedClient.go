a65e22d47349e41163a0435347cde5c1b8a4476f
Eduardo <edumadrim@gmail.com>
2021-06-28 11:14:01 -0600

    U: Fix Decorator Structure

--- API/WebServer/Controllers/Decorator/PrizedClient.go
+++ API/WebServer/Controllers/Decorator/PrizedClient.go
@@ -3,9 +3,8 @@ package Decorator
 type PrizedClient struct {
 	MembershipNumber int
 	Stars            int
-	claim            PrizeClaimer
 }
 
 func (p *PrizedClient) AwardPrize() {
-	p.claim.AwardPrize()
+
 }
